ad5e53e6ed3663bb1629836a07b99f16b4fcc227
rajkonkret <rajkonkret660@gmail.com>
2025-08-11 15:32:06 +0200

    [1 17] update ex3

--- dzien1/ex3.py
+++ dzien1/ex3.py
@@ -9,6 +9,7 @@ ws = wb['vgsales']
 row_position = 2
 col_position = 7
 
+# zsumowanie kolumn  7 do 10 z wiersza 2
 total_sales = ((ws.cell(row=row_position, column=col_position).value)
                + (ws.cell(row=row_position, column=col_position + 1).value)
                + (ws.cell(row=row_position, column=col_position + 2).value)
