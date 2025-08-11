61033ec862cbdc10a4651f35c94b54dacc4dac77
rajkonkret <rajkonkret660@gmail.com>
2025-08-11 12:25:25 +0200

    [1 6] update pierwszy.py, human readable

--- dzien1/pierwszy.py
+++ dzien1/pierwszy.py
@@ -84,4 +84,54 @@ nowy_tekst = tekst.upper()  # zapamiętanie zmiany w nowej zmiennej
 print(nowy_tekst)  # wypisanie zmiany z tej zmiennej, WITAJ ŚWIECIE
 
 zmienna1 = "GROSS"
-zmienna2 = "groẞ"
+zmienna2 = "groẞ"  # gross
+
+print(zmienna1 == zmienna2)  # == porównanie, False
+# ctrl d - powielanie linii
+print(zmienna1.lower() == zmienna2.lower())  # == porównanie, False
+print(zmienna1.casefold() == zmienna2.casefold())  # == porównanie, True
+# True, False - prawda, fałsz, zmienna logiczna
+
+print(1 != 0)  # czy jeden jest rózne od zera? True - tak
+
+name = "Radek"
+# f - string - sformatowany tekst
+print(f"Nazywam się {name}")  # Nazywam się Radek, w klamerki zostanie umieszczona wartośc zmiennej
+a = 4.567
+print(f"Liczba: {a}")  # Liczba: 4.567
+print(f"Liczba: {a:.2f}")  # zaokrąglona do dwóch miejsc po przecinku, Liczba: 4.57
+
+print("liczba:", a)  # liczba: 4.567
+
+# %f - float, %s - string
+print("Liczba: %f" % a)  # Liczba: 4.567000
+print("Liczba: %.2f" % a)  # Liczba: Liczba: 4.57
+# print("Liczba: %f" % "Radek") # TypeError: must be real number, not str, walidacja typów
+
+print("""
+Tekst
+    wielolinijkowy""")
+# "Tekst
+#     wielolinijkowy"
+
+"""Komentarz
+    wielolinijkowy
+    Komentarz dokumentacyjnyw"""
+
+print(100 / 3)  # 33.333333333333336 -> float, dzielenie
+print(100 // 3)  # część całkowita z dzielenia 33
+print(100 % 3)  # modulo, reszta z dzielenia, reszta 1
+print(10 % 3)  # reszta 1
+
+zysk = 890123456654
+# human readable
+print(f"Nasza duża liczba: {zysk:,}")  # Nasza duża liczba: 890,123,456,654
+print(f"Nasza duża liczba: {zysk:_}")  # Nasza duża liczba: 890_123_456_654
+# old, new podpowiedzi z pycharma jakie są nazwy parametrów
+# nie przepisujemy!!!
+# replace() - zamiana old na new
+print(f"Nasza duża liczba: {zysk:_}".replace("_", " "))  # Nasza duża liczba: 890 123 456 654
+
+zysk = 890_098_765_432
+print(type(zysk))  # <class 'int'>, całkowita
+print(zysk)  # 890098765432
