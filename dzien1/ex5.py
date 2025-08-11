5453bacfa68e749f83c5a049e526642d29c7e4bf
rajkonkret <rajkonkret660@gmail.com>
2025-08-11 16:30:25 +0200

    [1 20] update ex5

--- dzien1/ex5.py
+++ dzien1/ex5.py
@@ -14,15 +14,19 @@ ws.append(new_row)
 # wb.save(filename)
 # wb.close()
 
+wb = openpyxl.load_workbook(filename)
+ws = wb.active
+
+ws = wb['vgsales']
 # odczyt wiersza z arkusza
 ws = wb['vgsales']
 values = [ws.cell(row=ws.max_row, column=i).value for i in range(1, ws.max_column + 1)]
 print(values)
 # [1, 'The Legend of Zelda', 1986, 'Action', 'Nintendo', 3.74, 0.93, 1.69, 0.14, 6.51, 6.5, None]
 
-# usunięcie jedego wiersza (ostatni)
-ws.delete_rows(ws.max_row - 1, 1)
-ws.delete_rows(ws.max_row + 1, 1)
+# # usunięcie jedego wiersza (ostatni)
+# ws.delete_rows(ws.max_row - 1, 1)
+# ws.delete_rows(ws.max_row + 1, 1)
 ws.delete_rows(ws.max_row, 1)
 
 wb.save(filename)
