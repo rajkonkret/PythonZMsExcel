ae41aff4dd655a0667ec7eb84118161564f58f6f
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 13:43:08 +0200

    [2 22] add ex14

---
+++ dzien2/ex14.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# wstawienie tabeli do arkusza
+
+df = pd.DataFrame({
+    "Imie": ['Jan', "Anna", 'Tomek'],
+    "Wiek": [29, 24, 35],
+    "Miasto": ["Warszawa", "Wrocław", "Legnica"]
+})
+
+
