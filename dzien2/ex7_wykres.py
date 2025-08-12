d39adb64e324df115bd255d6e18e7cd2d1e083bd
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 10:32:10 +0200

    [2 9] update ex7

--- dzien2/ex7_wykres.py
+++ dzien2/ex7_wykres.py
@@ -1,5 +1,32 @@
+from multiprocessing.managers import BarrierProxy
+
 import openpyxl
 from openpyxl.chart import Reference, BarChart
 
 wb = openpyxl.load_workbook('video2.xlsx')
 ws = wb['Total Sales by Genre']
+
+# wycinamy dane z excela potrzebne do wykresu
+values = Reference(ws,
+                   min_col=2,
+                   max_col=2,
+                   min_row=1,
+                   max_row=13)
+
+cats = Reference(ws,
+                 min_col=1,
+                 max_col=1,
+                 min_row=2,
+                 max_row=13)
+
+# tworzenie wykresu, słupkowy
+chart = BarChart()
+chart.add_data(values, titles_from_data=True)
+chart.set_categories(cats)
+chart.title = "Total Sales"
+chart.x_axis.title = "Genre"
+chart.y_axis.title = "Total Sales by Genre"
+ws.add_chart(chart, "D2")  # umiesczenie wykresu w komórce D2
+
+wb.save('video2.xlsx')
+wb.close()
