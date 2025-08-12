69d66c452703081e9694e9935fc6b86f48e7c509
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 11:23:44 +0200

    [2 12] add ex9

---
+++ dzien2/ex9.py
@@ -0,0 +1,9 @@
+import openpyxl
+
+from openpyxl.styles import Font, colors, PatternFill, Border, Side
+from openpyxl.formatting.rule import CellIsRule
+
+wb = openpyxl.load_workbook('video2.xlsx')
+ws = wb.active
+
+ws = wb['Video Games Sales Data']
