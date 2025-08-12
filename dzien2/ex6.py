f1cc72a680268a799500da5b30341829b7524bfe
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 09:41:11 +0200

    [2 3] add ex6

---
+++ dzien2/ex6.py
@@ -0,0 +1,12 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('video2.xlsx')
+
+ws = wb['vgsales']
+
+ws['P1'] = 'Average Sales'
+# ws['P2'] = '= AVERAGE(K2:K16199)'
+ws['P2'] = '= AVERAGE(L2:L16199)'  # =ŚREDNIA(L2:L16199)
+
+wb.save('video2.xlsx')
+wb.close()
