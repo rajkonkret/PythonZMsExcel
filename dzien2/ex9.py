573022eb7f8df276e6f3d91c55799a9b2e16a184
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 11:36:04 +0200

    [2 12a] update ex9

--- dzien2/ex9.py
+++ dzien2/ex9.py
@@ -7,3 +7,30 @@ wb = openpyxl.load_workbook('video2.xlsx')
 ws = wb.active
 
 ws = wb['Video Games Sales Data']
+
+# FF0000 -> RGB -> R=FF G=0 B=0
+ws['A1'].font = Font(color='FF0000', bold=True, size=12)  # red
+ws['A2'].font = Font(color='0000FF')  # blue
+
+ws['A1'].fill = PatternFill('lightVertical', start_color='38e3ff')
+# FILL_NONE = 'none'
+# FILL_SOLID = 'solid'
+# FILL_PATTERN_DARKDOWN = 'darkDown'
+# FILL_PATTERN_DARKGRAY = 'darkGray'
+# FILL_PATTERN_DARKGRID = 'darkGrid'
+# FILL_PATTERN_DARKHORIZONTAL = 'darkHorizontal'
+# FILL_PATTERN_DARKTRELLIS = 'darkTrellis'
+# FILL_PATTERN_DARKUP = 'darkUp'
+# FILL_PATTERN_DARKVERTICAL = 'darkVertical'
+# FILL_PATTERN_GRAY0625 = 'gray0625'
+# FILL_PATTERN_GRAY125 = 'gray125'
+# FILL_PATTERN_LIGHTDOWN = 'lightDown'
+# FILL_PATTERN_LIGHTGRAY = 'lightGray'
+# FILL_PATTERN_LIGHTGRID = 'lightGrid'
+# FILL_PATTERN_LIGHTHORIZONTAL = 'lightHorizontal'
+# FILL_PATTERN_LIGHTTRELLIS = 'lightTrellis'
+# FILL_PATTERN_LIGHTUP = 'lightUp'
+# FILL_PATTERN_LIGHTVERTICAL = 'lightVertical'
+# FILL_PATTERN_MEDIUMGRAY = 'mediumGray'
+wb.save('video2.xlsx')
+wb.close()
