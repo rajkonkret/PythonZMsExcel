24b4f1cdbab8ef61b196a5f530c180eac5308f86
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 13:51:28 +0200

    [2 23] update ex14

--- dzien2/ex14.py
+++ dzien2/ex14.py
@@ -8,4 +8,28 @@ df = pd.DataFrame({
     "Miasto": ["Warszawa", "Wrocław", "Legnica"]
 })
 
+# kontekst menadżer
+with pd.ExcelWriter("Tabela.xlsx", engine="xlsxwriter") as writer:
+    df.to_excel(writer, sheet_name="Osoby", startrow=1, header=False, index=False)
 
+    workbook = writer.book
+    worksheet = writer.sheets["Osoby"]
+
+    header_format = workbook.add_format({
+        'bold': True,
+        "text_wrap": True,
+        'valign': '#D7E4BC',
+        'border': 1
+    })
+    # enumerate() - numeruje kolekcje
+    for col_num, value in enumerate(df.columns.values):
+        worksheet.write(0, col_num, value, header_format)
+
+    (max_row, max_col) = df.shape  # odczyt rozmiaru tabeli (Dataframa)
+    # chr() - znak o kodzie ascii chr(65) to A
+    cell_range = f"A1:{chr(65 + max_col - 1)}{max_row + 1}"
+
+    worksheet.add_table(cell_range, {
+        'name': "TabelaOsoby",
+        "columns": [{"header": col} for col in df.columns]  # kolumny z dataframe
+    })
