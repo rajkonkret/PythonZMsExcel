d193dd76d1e558d69400fe5ad65d25f805943851
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 13:57:47 +0200

    [2 26] update ex15

--- dzien2/ex15.py
+++ dzien2/ex15.py
@@ -1,3 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("")
+df = pd.read_csv("height_file.csv")
+print(df)
+#        Name  Height
+# 0    Aditya     179
+# 1    Sameer     181
+# 2  Dharwish     170
+# 3      Joel     167
+
+writer = pd.ExcelWriter('excel_from_csv.xlsx', engine="xlsxwriter")
+
+df.to_excel(writer, sheet_name="first_sheet", index=False)
+writer.close()
