2793e564e2e754482de43b79acb723432d25df4a
rajkonkret <rajkonkret660@gmail.com>
2025-08-12 10:11:23 +0200

    [2 6] update ex6, add ceiling

--- dzien2/ex6.py
+++ dzien2/ex6.py
@@ -23,3 +23,21 @@ ws['R2'] = '=COUNTIF(E2:E16620, "sports")'
 wb.save('video2.xlsx')
 wb.close()
 
+ws['S1'] = 'Total sports Sales'
+# ws['S2'] = '=SUMIF(E2:E16220, "Sports", K2:K16220)'
+ws['S2'] = '=SUMIF(E2:E16220, "Sports", L2:L16220)'
+
+wb.save('video2.xlsx')
+wb.close()
+
+ws['T1'] = "Rounded Sum of Sports Sales"
+ws['T2'] = '=CEILING(S2, 25)'  # do najblizszej 25, 1308.9 -> 1325
+
+wb.save('video2.xlsx')
+wb.close()
+
+ws['U1'] = "Rounded Sum of Sports Sales"
+ws['U2'] = '=CEILING(S2, 1)'  # do najblizszej całkowitej, 1308.9 -> 1309
+
+wb.save('video2.xlsx')
+wb.close()
